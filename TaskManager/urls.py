55447193c6cc5ea541adf06ed61bb6c50136b9da
Sergii Kantor <sergiy.kantor@gmail.com>
2022-11-10 17:49:20 +0200

    add footer and correct sidebar

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -4,15 +4,6 @@ from TaskManager.views import index
 
 urlpatterns = [
     path("", index, name="index"),
-    # path(""),
-    # path(""),
-    # path(""),
-    # path(""),
-    # path(""),
-    # path(""),
-    # path(""),
-    # path(""),
-    # path(""),
 ]
 
 app_name = "TaskManager"
